ef77c4d903c1b44a05734244bfdd089fa13ddce0
kendra rayne <raynekendra@gmail.com>
2020-02-03 21:10:30 -0500

    Added new properties

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Kendra McVay on 2/3/20.
+//  Copyright © 2020 Kendra McVay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
